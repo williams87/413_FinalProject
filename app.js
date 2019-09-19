a048cb771250dea9973400d5d842f2da697a626f
Roman Lysecky <roman@lysecky.com>
2019-09-18 17:52:18 -0700

    Added participation route.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var participationRouter = require('./routes/participation');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/participation', participationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
